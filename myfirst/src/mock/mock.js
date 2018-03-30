c8fa6cb532b8d469541169abcf7b55e4d3c1c42d
fanlz <fanlz@chianoly.com>
2018-03-30 17:25:48 +0800

    myfirst

---
+++ myfirst/src/mock/mock.js
@@ -0,0 +1,19 @@
+var Mock = require('mockjs')
+const Random = Mock.Random
+const produceDate = function (opt) {
+  console.log('opt', opt)
+  let articles = []
+  for (let i = 0; i < 30; i++) {
+    let newAricleObject = {
+      title: Random.csentence(5, 30),
+      thumbnail_pic_s: Random.dataImage('300x250', 'mock的图片'),
+      author_name: Random.cname(),
+      date: Random.date() + '' + Random.time()
+    }
+    articles.push(newAricleObject)
+  }
+  return {
+    data: articles
+  }
+}
+Mock.mock('/news', /post|get/i, produceDate)
